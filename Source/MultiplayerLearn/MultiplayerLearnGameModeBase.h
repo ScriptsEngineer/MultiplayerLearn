c4832f935a06b9e49f7342f2ef46c72a3ebe5e4a
Rafael Correa <scriptsengineer@gmail.com>
2020-02-04 16:16:21 -0300

    Add project

---
+++ Source/MultiplayerLearn/MultiplayerLearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerLearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERLEARN_API AMultiplayerLearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
